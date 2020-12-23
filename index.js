060edc2e96ceecfbe936f7515c254aa5f7a3ebc4
Artsiom Yermakou <artem_ermakov_1999@mail.ru>
2020-12-23 09:52:18 +0300

    1.2

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const obj = require("./user");
+const http = require("http");
 
-console.log(obj.user);
+const server = http.createServer((req, res) => {
+    console.log(req.url)
 
-obj.sayHello();
+    res.write(`<h1>Hello from Node.js 1</h1>`);
+    res.write(`<h2>Hello from Node.js 2</h2>`);
+    res.write(`<h3>Hello from Node.js 3</h3>`);
+    res.end(`
+<div style="width: 200px; font-size: 20px">
+<h1 style="color: red">Test</h1>
+</div>
+`);
+})
+
+server.listen(3000, () => {
+    console.log("Server is running...")
+});
